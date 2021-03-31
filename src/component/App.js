faf7e5eaaa93d5ae0b148b6915369472606f190d
billy07001 <72130484+billy07001@users.noreply.github.com>
2021-04-01 01:11:24 +0800

    sec commit

--- src/component/App.js
+++ src/component/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Table } from 'react-bootstrap';
 
 class App extends Component {
     constructor(props){
@@ -19,8 +20,16 @@ class App extends Component {
                    items: json
                })
           });
+        if (this.scroll) {
+            this.scroll.addEventListener("scroll", e => {
+              const { clientHeight, scrollHeight, scrollTop } = e.target;
+              // const { clientHeight, scrollHeight, scrollTop } = this.scroll;
+       
+              const isBottom = scrollTop + clientHeight + 20 > scrollHeight;
+              console.log(scrollTop, clientHeight, scrollHeight, isBottom);
+        });   
+     }
     }
-  
     render(){
 
     var {isLoaded, items} = this.state;
@@ -32,13 +41,16 @@ class App extends Component {
             return(
                 <div>
                     <h2> Welcome to the 交通部觀光景點</h2>
-                    <ul>
+                    <Table striped bordered hover size="sm">
+                      <tbody>
                         {items.slice(0,this.state.spotLimit).map((item,index) => (
-                            <li key={index}>
-                                {item.Name}
-                            </li>
+                            <tr key={index}>
+                                <td>{item.Name}</td>
+                                <td>{item.DescriptionDetail}</td>
+                            </tr>
                         ))};
-                    </ul>
+                      </tbody>
+                    </Table>
                 </div>
             )
         
